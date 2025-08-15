d5ec7e5f69076508341c7f6dbef27d89b3261888
shivapand <shiva.pandit@ecolibrium.io>
2025-08-15 18:20:15 +0530

    first commit

---
+++ lua/plugins/kanagawa.lua
@@ -0,0 +1,6 @@
+return {
+  'https://github.com/rebelot/kanagawa.nvim',
+  config = function()
+    vim.cmd.colorscheme('kanagawa-wave')
+  end
+}
---
+++ lua/plugins/mason-lspconfig.lua
@@ -0,0 +1,39 @@
+return {
+  "mason-org/mason-lspconfig.nvim",
+  dependencies = {
+    {
+      { "mason-org/mason.nvim", opts = {} },
+      "neovim/nvim-lspconfig",
+      "WhoIsSethDaniel/mason-tool-installer.nvim",
+    }
+  },
+  config = function()
+    require('mason-lspconfig').setup({
+      vim.diagnostic.config({
+        severity_sort = true,
+        float = { border = 'rounded', source = 'if_many' },
+        signs = {
+          text = {
+            [vim.diagnostic.severity.ERROR] = '󰅚 ',
+            [vim.diagnostic.severity.WARN] = '󰀪 ',
+            [vim.diagnostic.severity.INFO] = '󰋽 ',
+            [vim.diagnostic.severity.HINT] = '󰌶 ',
+          },
+        },
+        update_in_insert = true,
+        virtual_text = false
+      })
+    })
+
+    require("mason-tool-installer").setup({ 
+      ensure_installed = {
+        'lua-language-server',
+        'eslint-lsp',
+        'typescript-language-server',
+        'prettier',
+        'emmet-language-server',
+        'stylelint-lsp'
+      }
+    })
+  end
+}
